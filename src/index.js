777e8de8ce4711b69dcba80ef18650d1e35a20e2
Nigel Wade <nigel.wade@zonal.co.uk>
2017-10-25 17:57:46 +0100

    Adds basic rgl component

--- src/index.js
+++ src/index.js
@@ -8,9 +8,19 @@ const ReactGridLayout = WidthProvider(RGL);
 
 
 export class MyGrid extends React.Component{
+    
     render (){
+        const layout = [
+            { i: 'a', x: 0, y: 0, w: 1, h: 2, static: true },
+            { i: 'b', x: 1, y: 0, w: 3, h: 2, minW: 2, maxW: 4 },
+            { i: 'c', x: 4, y: 0, w: 1, h: 2 }
+        ];
         return (
-            <div>Hello world</div>
+            <ReactGridLayout className="layout" layout={layout} cols={12} rowHeight={100}>
+                <div key="a">a</div>
+                <div key="b">b</div>
+                <div key="c">c</div>
+            </ReactGridLayout>
         )
     }
 }
